67ad98542cd23bc820a580750e6d484d3c7bab4a
awaismajeed333 <okaz.s333s@gmail.com>
2021-02-21 16:50:13 +0500

    Add project files.

---
+++ RR_RoadWays_Services/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RR_RoadWays_Services.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
