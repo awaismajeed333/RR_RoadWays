c266d46af5528ffb8e68ab17c34c0afa4e75bcb5
awaismajeed333 <okaz.s333s@gmail.com>
2021-04-15 00:46:24 +0500

    feedback and maintenance report added

--- RR_RoadWays_Services/Controllers/ReportsController.cs
+++ RR_RoadWays_Services/Controllers/ReportsController.cs
@@ -15,8 +15,13 @@ namespace RR_RoadWays_Services.Controllers
         public IActionResult OilShop()
         {
             var context = new RRRoadwaysDBContext();
-            ViewBag.vehicleId = new SelectList(context.Vehicle.Where(x => x.IsDeleted == false).ToList(), "Id", "VehicleNumber");
-            ViewBag.OilShop = new SelectList(context.Station.Where(x => x.StationType.ToLower().Contains("oilshop")).ToList(), "Id", "Name");
+            List<Vehicle> vehiclelist = context.Vehicle.Where(x => x.IsDeleted == false).ToList();
+            vehiclelist.Insert(0, new Vehicle() { Id = -1, VehicleNumber = "All" });
+            List<Station> OilShoplist = context.Station.Where(x => x.StationType.ToLower().Contains("pump")).ToList();
+            OilShoplist.Insert(0, new Station() { Id = -1, Name = "All" });
+
+            ViewBag.vehicleId = new SelectList(vehiclelist, "Id", "VehicleNumber");
+            ViewBag.OilShop = new SelectList(OilShoplist, "Id", "Name");
             return View();
         }
 
@@ -26,7 +31,8 @@ namespace RR_RoadWays_Services.Controllers
             var context = new RRRoadwaysDBContext();
             int oildshopid = Convert.ToInt32(data.OilShopId);
             int vehicleNumber = Convert.ToInt32(data.VehicleNumber);
-            if (data.VehicleNumber == null)
+            List<OilShopReport> griddata = new List<OilShopReport>();
+            if (data.VehicleNumber == "-1" && data.OilShopId == "-1")
             {
                 var vouchers = context.Voucher.Join(context.Vehicle, vo => vo.VehicleNumber, v => v.Id, (vo, v) => new
                 {
@@ -34,11 +40,9 @@ namespace RR_RoadWays_Services.Controllers
                     Date = vo.CreatedDate,
                     Vehicle = v.VehicleNumber,
                     Amount = vo.OilAmount
-                }).Where(c => c.OilShopId == oildshopid
-                && c.Date.Value.Date >= data.StartDate.Date
+                }).Where(c => c.Date.Value.Date >= data.StartDate.Date
                 && c.Date.Value.Date <= data.EndDate.Date).ToList();
 
-                List<OilShopReport> griddata = new List<OilShopReport>();
                 if (vouchers.Count > 0)
                 {
                     foreach (var item in vouchers)
@@ -55,9 +59,66 @@ namespace RR_RoadWays_Services.Controllers
                     }
                 }
 
-                return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
             }
-            else {
+            else if (data.VehicleNumber != "-1" && data.OilShopId == "-1") {
+                var vouchers = context.Voucher.Join(context.Vehicle, vo => vo.VehicleNumber, v => v.Id, (vo, v) => new
+                {
+                    OilShopId = vo.OilShopId,
+                    Date = vo.CreatedDate,
+                    VehicleNumber = v.Id,
+                    Vehicle = v.VehicleNumber,
+                    Amount = vo.OilAmount
+                }).Where(c => c.VehicleNumber == vehicleNumber
+                && c.Date.Value.Date >= data.StartDate.Date
+                && c.Date.Value.Date <= data.EndDate.Date).ToList();
+
+                if (vouchers.Count > 0)
+                {
+                    foreach (var item in vouchers)
+                    {
+                        OilShopReport obj = new OilShopReport()
+                        {
+                            SerialNo = griddata.Count + 1,
+                            Date = item.Date.Value.Date,
+                            Amount = item.Amount,
+                            VehicleNumber = item.Vehicle
+                        };
+
+                        griddata.Add(obj);
+                    }
+                }
+            }
+            else if (data.VehicleNumber == "-1" && data.OilShopId != "-1")
+            {
+                var vouchers = context.Voucher.Join(context.Vehicle, vo => vo.VehicleNumber, v => v.Id, (vo, v) => new
+                {
+                    OilShopId = vo.OilShopId,
+                    Date = vo.CreatedDate,
+                    VehicleNumber = v.Id,
+                    Vehicle = v.VehicleNumber,
+                    Amount = vo.OilAmount
+                }).Where(c => c.OilShopId == oildshopid
+                && c.Date.Value.Date >= data.StartDate.Date
+                && c.Date.Value.Date <= data.EndDate.Date).ToList();
+
+                if (vouchers.Count > 0)
+                {
+                    foreach (var item in vouchers)
+                    {
+                        OilShopReport obj = new OilShopReport()
+                        {
+                            SerialNo = griddata.Count + 1,
+                            Date = item.Date.Value.Date,
+                            Amount = item.Amount,
+                            VehicleNumber = item.Vehicle
+                        };
+
+                        griddata.Add(obj);
+                    }
+                }
+            }
+            else
+            {
                 var vouchers = context.Voucher.Join(context.Vehicle, vo => vo.VehicleNumber, v => v.Id, (vo, v) => new
                 {
                     OilShopId = vo.OilShopId,
@@ -70,7 +131,6 @@ namespace RR_RoadWays_Services.Controllers
                 && c.Date.Value.Date >= data.StartDate.Date
                 && c.Date.Value.Date <= data.EndDate.Date).ToList();
 
-                List<OilShopReport> griddata = new List<OilShopReport>();
                 if (vouchers.Count > 0)
                 {
                     foreach (var item in vouchers)
@@ -86,16 +146,19 @@ namespace RR_RoadWays_Services.Controllers
                         griddata.Add(obj);
                     }
                 }
-
-                return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
             }
+            return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
         }
 
         public IActionResult Pump()
         {
             var context = new RRRoadwaysDBContext();
-            ViewBag.vehicleId = new SelectList(context.Vehicle.Where(x => x.IsDeleted == false).ToList(), "Id", "VehicleNumber");
-            ViewBag.PumpId = new SelectList(context.Station.Where(x => x.StationType.ToLower().Contains("pump")).ToList(), "Id", "Name");
+            List<Vehicle> pumplist = context.Vehicle.Where(x => x.IsDeleted == false).ToList();
+            pumplist.Insert(0, new Vehicle() { Id = -1, VehicleNumber = "All" });
+            List<Station> stationlist = context.Station.Where(x => x.StationType.ToLower().Contains("pump")).ToList();
+            stationlist.Insert(0, new Station() { Id = -1, Name = "All" });
+            ViewBag.vehicleId = new SelectList(pumplist, "Id", "VehicleNumber");
+            ViewBag.PumpId = new SelectList(stationlist, "Id", "Name");
             return View();
         }
 
@@ -105,7 +168,8 @@ namespace RR_RoadWays_Services.Controllers
             var context = new RRRoadwaysDBContext();
             int pumpid = Convert.ToInt32(data.PumpId);
             int vehicleNumber = Convert.ToInt32(data.VehicleNumber);
-            if (data.VehicleNumber == null)
+            List<PumpReport> griddata = new List<PumpReport>();
+            if (data.VehicleNumber == "-1" && data.PumpId == "-1")
             {
 
                 var pumpdata = context.Vehicle
@@ -118,7 +182,7 @@ namespace RR_RoadWays_Services.Controllers
                     vdd => vdd.VoucherId,
                     (c, vdd) => new
                     {
-                        PumpId= vdd.StationId,
+                        PumpId = vdd.StationId,
                         Date = c.vo.CreatedDate,
                         VehicleNumber = c.v.Id,
                         Vehicle = c.v.VehicleNumber,
@@ -126,12 +190,10 @@ namespace RR_RoadWays_Services.Controllers
                         Rate = vdd.Rate,
                         Amount = vdd.Amount,
                         OilAndOthers = vdd.OilAndOthers,
-                    }).Where(c => c.PumpId == pumpid
-                && c.Date.Value.Date >= data.StartDate.Date
+                    }).Where(c => c.Date.Value.Date >= data.StartDate.Date
                 && c.Date.Value.Date <= data.EndDate.Date).ToList();
 
 
-                List<PumpReport> griddata = new List<PumpReport>();
                 if (pumpdata.Count > 0)
                 {
                     foreach (var item in pumpdata)
@@ -152,8 +214,97 @@ namespace RR_RoadWays_Services.Controllers
                         griddata.Add(obj);
                     }
                 }
+            }
+            else if (data.VehicleNumber == "-1" && data.PumpId != "-1")
+            {
+                var pumpdata = context.Vehicle
+                .Join(context.Voucher,
+                v => v.Id,
+                vo => vo.VehicleNumber,
+                (v, vo) => new { v, vo })
+                .Join(context.VoucherDieselDetails,
+                c => c.vo.Id,
+                vdd => vdd.VoucherId,
+                (c, vdd) => new
+                {
+                    PumpId = vdd.StationId,
+                    Date = c.vo.CreatedDate,
+                    VehicleNumber = c.v.Id,
+                    Vehicle = c.v.VehicleNumber,
+                    Litre = vdd.Litre,
+                    Rate = vdd.Rate,
+                    Amount = vdd.Amount,
+                    OilAndOthers = vdd.OilAndOthers,
+                }).Where(c => c.PumpId == pumpid
+                && c.Date.Value.Date >= data.StartDate.Date
+                && c.Date.Value.Date <= data.EndDate.Date).ToList();
 
-                return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
+                if (pumpdata.Count > 0)
+                {
+                    foreach (var item in pumpdata)
+                    {
+                        PumpReport obj = new PumpReport()
+                        {
+                            SerialNo = griddata.Count + 1,
+                            Date = item.Date.Value.Date,
+                            Vehicle = item.Vehicle,
+                            Litre = item.Litre,
+                            Rate = item.Rate,
+                            Amount = (item.Litre * item.Rate),
+                            OilAndOther = item.OilAndOthers,
+                            OilAmount = item.Amount,
+                            Total = (item.Litre * item.Rate) + item.Amount,
+                        };
+
+                        griddata.Add(obj);
+                    }
+                }
+            }
+            else if (data.VehicleNumber != "-1" && data.PumpId == "-1")
+            {
+                var pumpdata = context.Vehicle
+                   .Join(context.Voucher,
+                   v => v.Id,
+                   vo => vo.VehicleNumber,
+                   (v, vo) => new { v, vo })
+                   .Join(context.VoucherDieselDetails,
+                   c => c.vo.Id,
+                   vdd => vdd.VoucherId,
+                   (c, vdd) => new
+                   {
+                       PumpId = vdd.StationId,
+                       Date = c.vo.CreatedDate,
+                       VehicleNumber = c.v.Id,
+                       Vehicle = c.v.VehicleNumber,
+                       Litre = vdd.Litre,
+                       Rate = vdd.Rate,
+                       Amount = vdd.Amount,
+                       OilAndOthers = vdd.OilAndOthers,
+                   }).Where(c => c.VehicleNumber == vehicleNumber
+               && c.Date.Value.Date >= data.StartDate.Date
+               && c.Date.Value.Date <= data.EndDate.Date).ToList();
+
+
+                if (pumpdata.Count > 0)
+                {
+                    foreach (var item in pumpdata)
+                    {
+                        PumpReport obj = new PumpReport()
+                        {
+                            SerialNo = griddata.Count + 1,
+                            Date = item.Date.Value.Date,
+                            Vehicle = item.Vehicle,
+                            Litre = item.Litre,
+                            Rate = item.Rate,
+                            Amount = (item.Litre * item.Rate),
+                            OilAndOther = item.OilAndOthers,
+                            OilAmount = item.Amount,
+                            Total = (item.Litre * item.Rate) + item.Amount,
+                        };
+
+                        griddata.Add(obj);
+                    }
+                }
             }
             else
             {
@@ -181,7 +332,6 @@ namespace RR_RoadWays_Services.Controllers
                && c.Date.Value.Date <= data.EndDate.Date).ToList();
 
 
-                List<PumpReport> griddata = new List<PumpReport>();
                 if (pumpdata.Count > 0)
                 {
                     foreach (var item in pumpdata)
@@ -203,8 +353,96 @@ namespace RR_RoadWays_Services.Controllers
                     }
                 }
 
-                return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
             }
+            return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
+        }
+
+        public IActionResult Maintenance()
+        {
+            var context = new RRRoadwaysDBContext();
+            List<Vehicle> vehiclelist = context.Vehicle.Where(x => x.IsDeleted == false).ToList();
+            vehiclelist.Insert(0, new Vehicle() { Id = -1, VehicleNumber = "All" });
+            ViewBag.vehicleId = new SelectList(vehiclelist, "Id", "VehicleNumber");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult getMaintenanceData(MaintenanceModel data) {
+            var context = new RRRoadwaysDBContext();
+            int vehicleNumber = Convert.ToInt32(data.VehicleNumber);
+            List<MaintenanceReport> griddata = new List<MaintenanceReport>();
+            if (data.VehicleNumber == "-1")
+            {
+                var maintenances = context.Maintenance
+                    .Join(context.Station, m => m.StationId, s => s.Id, (m, s) => new {m,s })
+                    .Join(context.Department, b => b.m.DepartmentId , d => d.Id, (b, d) => new
+                    {
+                        Date = b.m.MaintenanceDate,
+                        MaintenanceShop = b.s.Name,
+                        Department = d.DepartmentName,
+                        Description = b.m.Description,
+                        Maintenance = b.m.MaintenanceDesc,
+                        Amount = b.m.Amount
+                }).Where(c => c.Date.Value.Date >= data.StartDate.Date
+                && c.Date.Value.Date <= data.EndDate.Date).ToList();
+
+                if (maintenances.Count > 0)
+                {
+                    foreach (var item in maintenances)
+                    {
+                        MaintenanceReport obj = new MaintenanceReport()
+                        {
+                            SerialNo = griddata.Count + 1,
+                            Date = item.Date.Value.Date,
+                            MaintenanceShop = item.MaintenanceShop,
+                            Department = item.Department,
+                            Description = item.Description,
+                            Maintenance= item.Maintenance,
+                            Amount = item.Amount,
+                        };
+
+                        griddata.Add(obj);
+                    }
+                }
+
+            }
+            else
+            {
+                var maintenances = context.Maintenance
+                    .Join(context.Station, m => m.StationId, s => s.Id, (m, s) => new { m, s })
+                    .Join(context.Department, b => b.m.DepartmentId, d => d.Id, (b, d) => new
+                    {
+                        VehicleId = b.m.VehicleId,
+                        Date = b.m.MaintenanceDate,
+                        MaintenanceShop = b.s.Name,
+                        Department = d.DepartmentName,
+                        Description = b.m.Description,
+                        Maintenance = b.m.MaintenanceDesc,
+                        Amount = b.m.Amount
+                    }).Where(c => c.VehicleId == vehicleNumber
+                    && c.Date.Value.Date >= data.StartDate.Date
+                    && c.Date.Value.Date <= data.EndDate.Date).ToList();
+
+                if (maintenances.Count > 0)
+                {
+                    foreach (var item in maintenances)
+                    {
+                        MaintenanceReport obj = new MaintenanceReport()
+                        {
+                            SerialNo = griddata.Count + 1,
+                            Date = item.Date.Value.Date,
+                            MaintenanceShop = item.MaintenanceShop,
+                            Department = item.Department,
+                            Description = item.Description,
+                            Maintenance = item.Maintenance,
+                            Amount = item.Amount,
+                        };
+
+                        griddata.Add(obj);
+                    }
+                }
+            }
+            return Json(new { data = griddata }, new Newtonsoft.Json.JsonSerializerSettings());
         }
     }
 }
---
+++ RR_RoadWays_Services/Models/MaintenanceReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RR_RoadWays_Services.Models
+{
+    public class MaintenanceModel
+    {
+        public string VehicleNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public class MaintenanceReport {
+        public int SerialNo { get; set; }
+        public DateTime? Date { get; set; }
+        public string MaintenanceShop { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+        public string Maintenance { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
